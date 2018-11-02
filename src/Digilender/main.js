d9b6568a17dc34dd45889f7ad0832ad080c698f1
Mirko Messina <mirko.messina@student.ap.be>
2018-11-02 22:12:21 +0100

    Reformatted code

--- src/Digilender/main.js
+++ src/Digilender/main.js
@@ -1,17 +1,15 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const { Console } = require('console');
-
+const { app, BrowserWindow } = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   console.log("ayylmao");
-  mainWindow = new BrowserWindow({width: 800, height: 600, frame: false});
-  mainWindow.setMenu(null);
+  mainWindow = new BrowserWindow({ width: 800, height: 600, frame: false });
+  // mainWindow.setMenu(null);
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html');
