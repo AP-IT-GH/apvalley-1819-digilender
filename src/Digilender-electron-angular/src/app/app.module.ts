d6a60a00a0445cb2b36ad1362dd6374666b7c904
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-11-06 12:08:46 +0100

    Created home calender component

--- src/Digilender-electron-angular/src/app/app.module.ts
+++ src/Digilender-electron-angular/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeCalendarComponent } from './home-calendar/home-calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeCalendarComponent
   ],
   imports: [
     BrowserModule,
