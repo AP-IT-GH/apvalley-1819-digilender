979cf34f9b9f8f4e4c809999b50b0e8ee8053fdd
Jasmien De Clercq <jasmien.declercq@hotmail.com>
2018-11-07 02:11:48 +0100

    Er is nog iets mis met de weather service -hoe debuggen met electron?

--- src/Digilender-electron-angular/src/app/app.component.ts
+++ src/Digilender-electron-angular/src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { WeatherService } from './weather.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+ // var apiKey = 'a54b87c5a39fed1429dcbda86a35318f';
+  
+export class AppComponent implements OnInit {
   title = 'Digilender-electron-angular';
+
+  condition: string;
+  currentTemp: number;
+  maxTemp: number;
+  minTemp: number;
+  darkMode: boolean;
+
+ 
+  constructor(public weather: WeatherService){}
+              
+
+  ngOnInit() {
+    
+    var city = 'Antwerp';
+    /*
+     this.weather.getWeatherState(city).subscribe((data: string) => {
+        this.condition = data;
+      });
+    this.weather.getMinTemp(city).subscribe((data: number) => {
+      this.minTemp = data;
+    });
+    this.weather.getMaxTemp(city).subscribe((data: number) => {
+      this.maxTemp = data;
+    });  */
+    this.weather.getCurrentTemp(city).subscribe((data: number) => {
+      this.currentTemp = data;
+      console.log(this.currentTemp);
+    });
+    
+  }
 }
+ 
