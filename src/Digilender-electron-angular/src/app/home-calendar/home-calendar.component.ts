dc7e51fd4abb8de737586f82264290a7df00ac5b
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-11-13 16:33:10 +0100

    commit before merge into master

--- src/Digilender-electron-angular/src/app/home-calendar/home-calendar.component.ts
+++ src/Digilender-electron-angular/src/app/home-calendar/home-calendar.component.ts
@@ -36,45 +36,45 @@ export class HomeCalendarComponent implements OnInit {
         return time;
       };
 
-     // let containerEl: JQuery = $('#calendar');
+    // let containerEl: JQuery = $('#calendar');
       $('#calendar').fullCalendar({
         defaultView: 'agendaMonth',
-    groupByResource: true,
-    header: {
-      left: 'prev,next today',
-      center:'title',
-      right: 'agendaMonth,listThreeDay,agendaWeek,month'
-    },
-    views: {
-      listThreeDay: {
-        type: 'list',
-        duration: {
-          days: 31
-        }
-      },
-      agendaMonth: {
-        type: 'agendaDay',
-        minTime: getMinTime(),
-        maxTime: getMaxTime(),
-        slotDuration: '24:00:00',
-        slotLabelFormat: 'D MMMM YYYY',
-        buttonText: 'Week'
-      },      
-    },    
-    selectable: false,
-    nowIndicator: false,
-    allDaySlot: false,
-    resources: [
-      { id: '1', title: 'Bram' },
-      { id: '2', title: 'Tom' },
-      { id: '3', title: 'Bram' },
-      { id: '4', title: 'Bram' },
-      { id: '5', title: 'Bram' }
-    ],
-    events: 'https://fullcalendar.io/demo-events.json?with-resources=2'
-  });
-        
-      })
-    
+        groupByResource: true,
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'agendaMonth,listThreeDay,agendaWeek,month'
+        },
+        views: {
+          listThreeDay: {
+            type: 'list',
+            duration: {
+              days: 31
+            }
+          },
+          agendaMonth: {
+            type: 'agendaDay',
+            minTime: getMinTime(),
+            maxTime: getMaxTime(),
+            slotDuration: '24:00:00',
+            slotLabelFormat: 'D MMMM YYYY',
+            buttonText: 'Week'
+          },
+        },
+        selectable: false,
+        nowIndicator: false,
+        allDaySlot: false,
+        // resources: [
+        //   { id: '1', title: 'Bram' },
+        //   { id: '2', title: 'Tom' },
+        //   { id: '3', title: 'Tim' },
+        //   { id: '4', title: 'Elke' },
+        //   { id: '5', title: 'Mirko' }
+        // ],
+        events: 'https://fullcalendar.io/demo-events.json?with-resources=2'
+      });
+
+    })
+
   }
 }
