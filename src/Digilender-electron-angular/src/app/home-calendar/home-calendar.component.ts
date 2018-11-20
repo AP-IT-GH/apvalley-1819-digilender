649c0153b99e1cba2f6d598a87429ef32a4e1ca3
Antoine <antoinevmasure@gmail.com>
2018-11-20 15:31:31 +0100

    Revert "weekoverzicht data links"
    This reverts commit a96832a26a6d3d96a81015273342e27f9943b340.

--- src/Digilender-electron-angular/src/app/home-calendar/home-calendar.component.ts
+++ src/Digilender-electron-angular/src/app/home-calendar/home-calendar.component.ts
@@ -3,17 +3,15 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import 'fullcalendar';
 import 'fullcalendar-scheduler';
 
-
-
 @Component({
   selector: 'app-home-calendar',
   templateUrl: './home-calendar.component.html',
-  styleUrls: ['./home-calendar.component.css']
+  styleUrls: ['./home-calendar.component.scss']
 })
 export class HomeCalendarComponent implements OnInit {
   ngOnInit(): void {
-   
-     
+    $(function () {
+
       var getDaysInMonth = function () {
         var d = new Date();
         var year = d.getFullYear();
@@ -37,55 +35,46 @@ export class HomeCalendarComponent implements OnInit {
         var time = days + ".00:00:00";
         return time;
       };
-     
-      //let containerEl: JQuery = $('#calendar');
-     $(function(){
-       
-       let containerEl: JQuery=$('#calendar');
-       
-       containerEl.fullCalendar({
-         
-        
+
+     // let containerEl: JQuery = $('#calendar');
+      $('#calendar').fullCalendar({
         defaultView: 'agendaMonth',
-        groupByResource: true,
-        timezone:false,
-        header: {
-          left: 'prev,next today',
-          center:'title',
-          right: 'agendaMonth,listThreeDay,agendaWeek,month'
-        },
-        views: {
-          listThreeDay: {
-            type: 'list',
-            duration: {
-              days: 31
-            }
-          },
-          agendaMonth: {
-            type: 'agendaDay',
-            minTime: getMinTime(),
-            maxTime: getMaxTime(),
-            slotDuration: '24:00:00',
-            slotLabelFormat: 'D MMMM YYYY',
-            buttonText: 'Week'
-          },      
-        },    
-        selectable: false,
-        nowIndicator: false,
-        allDaySlot: false,
-        resources: [
-          { id: '1',
-           title: 'Bram'},
-          { id: '2', title: 'Tom' },
-          { id: '3', title: 'Bram' },
-          { id: '4', title: 'Bram' },
-          { id: '5', title: 'Bram' }
-        ],
-      
-      events: 'https://fullcalendar.io/demo-events.json?with-resources=2'
-      });
-       })
-  
-  
+    groupByResource: true,
+    header: {
+      left: 'prev,next today',
+      center:'title',
+      right: 'agendaMonth,listThreeDay,agendaWeek,month'
+    },
+    views: {
+      listThreeDay: {
+        type: 'list',
+        duration: {
+          days: 31
+        }
+      },
+      agendaMonth: {
+        type: 'agendaDay',
+        minTime: getMinTime(),
+        maxTime: getMaxTime(),
+        slotDuration: '24:00:00',
+        slotLabelFormat: 'D MMMM YYYY',
+        buttonText: 'Week'
+      },      
+    },    
+    selectable: false,
+    nowIndicator: false,
+    allDaySlot: false,
+    resources: [
+      { id: '1', title: 'Bram' },
+      { id: '2', title: 'Tom' },
+      { id: '3', title: 'Bram' },
+      { id: '4', title: 'Bram' },
+      { id: '5', title: 'Bram' }
+    ],
+    events: 'https://fullcalendar.io/demo-events.json?with-resources=2'
+  });
+        
+      })
+    
   }
 }
