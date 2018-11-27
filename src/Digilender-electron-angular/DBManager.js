82da5cc89386373c5c9cff0f510dfd8b831575a2
joeri temmerman <joeri.temmerman@hotmail.com>
2018-11-27 15:31:24 +0100

    fixed build

--- src/Digilender-electron-angular/DBManager.js
+++ src/Digilender-electron-angular/DBManager.js
@@ -1,32 +1,29 @@
 const Sequelize = require('sequelize');
 
 class DBManager{
-
-  sequelize = new Sequelize({
-    database: 'Digilender',
-    dialect: 'sqlite',
-    username: 'root',
-    password: '',
-    storage: 'Digilender.sqlite'
-  });
-
-  User = sequelize.define('User', {
-    name: {type: Sequelize.STRING, primaryKey: true},
-    calType: Sequelize.INTEGER,
-    login: Sequelize.STRING,
-    pass: Sequelize.STRING
-  });
-
-  Event = sequelize.define('Event', {
-    id: {type: Sequelize.UUID, primaryKey: true},
-    userName: Sequelize.STRING,
-    startDate: Sequelize.STRING,
-    stopDate: Sequelize.STRING,
-    Description: Sequelize.TEXT
-  });
-
   constructor(){
+    let sequelize = new Sequelize({
+      database: 'Digilender',
+      dialect: 'sqlite',
+      username: 'root',
+      password: '',
+      storage: 'Digilender.sqlite'
+    });
 
+    let User = sequelize.define('User', {
+      name: {type: Sequelize.STRING, primaryKey: true},
+      calType: Sequelize.INTEGER,
+      login: Sequelize.STRING,
+      pass: Sequelize.STRING
+    });
+
+    let Event = sequelize.define('Event', {
+      id: {type: Sequelize.UUID, primaryKey: true},
+      userName: Sequelize.STRING,
+      startDate: Sequelize.STRING,
+      stopDate: Sequelize.STRING,
+      Description: Sequelize.TEXT
+    });
   }
 
   getUsers(){
