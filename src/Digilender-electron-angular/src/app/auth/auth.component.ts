0d84b0274035763535c3817a367029c5cfd08d30
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-12-02 21:32:12 +0100

    redirect to calendar if user is logged

--- src/Digilender-electron-angular/src/app/auth/auth.component.ts
+++ src/Digilender-electron-angular/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -8,15 +9,24 @@ import { AuthService } from '../auth.service';
 })
 export class AuthComponent implements OnInit {
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService, private router: Router,  private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.isUserInlogged()
+  }
 
+  async isUserInlogged() {
+    const user = await this.auth.isLoggedIn()
+    if (user) {
+      this.router.navigate(['/calendar'], { relativeTo: this.route });
+    } else {
+      this.router.navigate(['/auth'], { relativeTo: this.route });
+    }
   }
 
   login() {
     this.auth.login()
-    
+    this.isUserInlogged()
   }
 
   logout(){
