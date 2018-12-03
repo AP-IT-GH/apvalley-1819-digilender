4671f9813d669f29e4ac9ab70ab835b1f26e03fb
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-12-03 20:04:02 +0100

    made express server and change loadURL to http url

--- src/Digilender-electron-angular/main.js
+++ src/Digilender-electron-angular/main.js
@@ -17,9 +17,7 @@ function createWindow() {
     // load the dist folder from Angular
     win.loadURL(
         url.format({
-            pathname: path.join(__dirname, `/dist/index.html`),
-            protocol: "file:",
-            slashes: true
+            pathname: 'http://localhost:4200'
         })
     );
 
---
+++ src/Digilender-electron-angular/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const http = require('http')
+const path = require('path')
+
+const app = express()
+
+const port = process.env.PORT || 4200;
+
+app.use(express.static(__dirname + '/dist'))
+
+app.get('/*', (req, res) => res.sendfile(path.join(__dirname)))
+
+const server = http.createServer(app)
+
+server.listen(port, () => console.log('Running on port ' + port))
