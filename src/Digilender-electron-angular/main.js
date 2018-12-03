01a94b1c376d94e78f884316a7e79ba70dcfcb8a
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-12-03 17:53:44 +0100

    set devTool on in electron

--- src/Digilender-electron-angular/main.js
+++ src/Digilender-electron-angular/main.js
@@ -24,7 +24,7 @@ function createWindow() {
     );
 
     // The following is optional and will open the DevTools:
-    // win.webContents.openDevTools()
+    win.webContents.openDevTools()
     win.on("closed", () => {
         win = null;
     });
--- src/Digilender-electron-angular/src/environments/environment.prod.ts
+++ src/Digilender-electron-angular/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC5s71KnhbfQpRlDMDf6gEqy6t4RiSY6_8",
+    authDomain: "digilender-a66ac.firebaseapp.com",
+    databaseURL: "https://digilender-a66ac.firebaseio.com",
+    projectId: "digilender-a66ac",
+    storageBucket: "digilender-a66ac.appspot.com",
+    messagingSenderId: "481745558533"
+  }
 };
