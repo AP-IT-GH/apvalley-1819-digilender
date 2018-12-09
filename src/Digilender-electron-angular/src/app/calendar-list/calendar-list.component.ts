519e32cdb59f80a7487eb644900767a7d038c2f5
Mirko Messina <mirko.messina@student.ap.be>
2018-12-09 18:45:40 +0100

    Removed clutter from CalendarList

--- src/Digilender-electron-angular/src/app/calendar-list/calendar-list.component.ts
+++ src/Digilender-electron-angular/src/app/calendar-list/calendar-list.component.ts
@@ -14,49 +14,18 @@ export class CalendarListComponent implements OnInit {
   constructor(public db: DatabaseService) { }
 
   ngOnInit(): void {
-    var me = this;
+    var self = this;
     $(function () {
-      var getDaysInMonth = function () {
-        var d = new Date();
-        var year = d.getFullYear();
-        var month = d.getMonth() + 1;
-        return new Date(year, month, 0).getDate();
-      };
-
-      //niet meer nodig door today-functie
-      var getMonthDay = function () {
-        var d = new Date();
-        return d.getDate();
-      };
-
-      var getMinTime = function () {
-        var time = $('#calendar-list').fullCalendar('today');
-        return time;
-      };
-
-      var getMaxTime = function () {
-        var days = 7;
-        var time = days + ".00:00:00";
-        return time;
-      };
-
-      // let containerEl: JQuery = $('#calendar');
       $('#calendar-list').fullCalendar({
         height: $(window).height() * 0.83,
-        defaultView: 'listThreeDay',
+        defaultView: 'listDay',
         groupByResource: true,
         header: false,
-        views: {
-          listThreeDay: {
-            type: 'list',
-            duration: {
-              days: 31
-            }
-          }
-        },
+        views: { listDay: {} },
         selectable: false,
         nowIndicator: true,
         allDaySlot: false,
+        listDayFormat: false,
         eventTextColor: 'white',
         // resources: [
         //   { id: '1', title: 'Bram' },
@@ -66,7 +35,7 @@ export class CalendarListComponent implements OnInit {
         //   { id: '5', title: 'Mirko' }
         // ],
         events: (start, end, timezone, callback) => {
-          me.db.getEvents(undefined).then((events) => {
+          self.db.getEvents(undefined).then((events) => {
             callback(events);
           });
         },
@@ -75,7 +44,8 @@ export class CalendarListComponent implements OnInit {
           date.utc()
         },
       });
-      //werkende optie om overschot onderaan calender weg te halen
+
+      // Werkende optie om overschot onderaan calender weg te halen
       $('#calendar-list').fullCalendar('option', 'contentHeight', "auto");
     })
   }
