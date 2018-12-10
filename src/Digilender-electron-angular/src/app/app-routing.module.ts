d5078ddac0070a15fbcc425b5d2bb8f1d0d19a15
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-12-10 21:35:41 +0100

    add google calendar sync to user settings

--- src/Digilender-electron-angular/src/app/app-routing.module.ts
+++ src/Digilender-electron-angular/src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
+import { AuthComponent } from './googleSyncCalendar/auth.component';
 import { HomeComponent } from './home/home.component';
-import { HomeCalendarComponent } from './calendar/calendar.component';
+import { calendarComponent } from './calendar/calendar.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  {path: 'calendar', component: HomeCalendarComponent},
-  {path: 'auth', component: AuthComponent},
-  {path: 'home', component: HomeComponent}
+  { path: 'calendar', component: calendarComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash:true})],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeCalendarComponent]
+export const routingComponents = [calendarComponent]
