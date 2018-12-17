084a14ef019d2ccecd9acec826b5beff55dd2c14
Mohammed L <mohammed.laghzaoui@outlook.com>
2018-12-17 19:16:45 +0100

    change builder to angular to run ng serve

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,7 +8,8 @@ import {
     MatCardModule,
     MatIconModule,
     MatDividerModule,
-    MatListModule
+    MatListModule,
+    MatTabsModule
 } from '@angular/material';
 
 @NgModule({
@@ -20,7 +21,8 @@ import {
         MatCardModule,
         MatIconModule,
         MatDividerModule,
-        MatListModule
+        MatListModule,
+        MatTabsModule
     ],
     exports: [
         MatButtonModule,
@@ -30,7 +32,8 @@ import {
         MatCardModule,
         MatIconModule,
         MatDividerModule,
-        MatListModule
+        MatListModule,
+        MatTabsModule
     ],
 })
 export class MaterialModule { }
