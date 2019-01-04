23a7ff2d50f4b96b923753b9aef03ac085b91251
Mohammed L <mohammed.laghzaoui@outlook.com>
2019-01-04 17:23:34 +0100

    set wifi component layout

--- src/app/wifi/wifi.component.ts
+++ src/app/wifi/wifi.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-wifi',
@@ -8,12 +8,50 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class WifiComponent implements OnInit {
 
-  constructor( private router: Router, private route: ActivatedRoute) { }
+  isLinear = true;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
 
   ngOnInit() {
-  }
-  goTo(pad:String):void{
-    this.router.navigate(['/'+ pad], { relativeTo: this.route });
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
 
+
+
+  dummySSIDs: Section[] = [
+    {
+      name: 'WiFi-2.4-1R82',
+      signal_dB: -40
+    },
+    {
+      name: 'telenet-32FE1',
+      signal_dB: -50
+    },
+    {
+      name: 'TelenetWiFree',
+      signal_dB: -60
+    },
+    {
+      name: 'telenet-55ST2',
+      signal_dB: -70
+    },
+    {
+      name: 'TELENETHOMESPOT',
+      signal_dB: -80
+    }
+  ]
+
+}
+
+
+export interface Section {
+  name: string;
+  signal_dB: number;
 }
