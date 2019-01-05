7f108c943879b3b834dfc5e50264069bc504a946
Mohammed L <mohammed.laghzaoui@outlook.com>
2019-01-05 16:14:23 +0100

    create intro setup home component

---
+++ src/app/intro-setup/intro-setup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro-setup',
+  templateUrl: './intro-setup.component.html',
+  styleUrls: ['./intro-setup.component.scss']
+})
+export class IntroSetupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
