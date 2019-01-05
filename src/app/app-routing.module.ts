de68fd56bef92949864bf2a51c93bf8973ad2682
Mohammed L <mohammed.laghzaoui@outlook.com>
2019-01-05 16:11:16 +0100

    change app routing for intro setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,32 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AuthComponent } from './googleSyncCalendar/auth.component';
-import { HomeCalendarComponent } from './calendar/calendar.component';
+import { CalendarComponent } from './calendar/calendar.component';
 import { OptionsComponent } from './options/options.component';
 import { UsersComponent } from './users/users.component';
 import { WifiComponent } from './wifi/wifi.component';
 import { LocatieComponent } from './locatie/locatie.component';
 import { ThemaComponent } from './thema/thema.component';
 import { SchermComponent } from './scherm/scherm.component';
+import { IntroSetupComponent } from "./intro-setup/intro-setup.component";
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/calendar', pathMatch: 'full' },
-  { path: 'calendar', component: HomeCalendarComponent },
-  { path: 'auth', component: AuthComponent },
-  { path: 'options', component: OptionsComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'wifi', component: WifiComponent },
-  { path: 'locatie', component: LocatieComponent },
-  { path: 'thema', component: ThemaComponent },
-  { path: 'scherm', component: SchermComponent }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'intro', component: IntroSetupComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      { path: 'calendar', component: CalendarComponent },
+      { path: 'options', component: OptionsComponent },
+      { path: 'users', component: UsersComponent },
+      { path: 'wifi', component: WifiComponent },
+      { path: 'locatie', component: LocatieComponent },
+      { path: 'thema', component: ThemaComponent },
+      { path: 'scherm', component: SchermComponent }
+    ]
+  },
+
 
 ];
 
@@ -27,4 +35,12 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeCalendarComponent]
+export const routingComponents = [CalendarComponent,
+  OptionsComponent,
+  UsersComponent,
+  WifiComponent,
+  LocatieComponent,
+  ThemaComponent,
+  SchermComponent,
+  IntroSetupComponent,
+  HomeComponent]
