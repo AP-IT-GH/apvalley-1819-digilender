4e2b7f80191afd827dd7cb07f862477701d8c764
Mohammed L <mohammed.laghzaoui@outlook.com>
2019-01-06 12:50:34 +0100

    add aditional step for accounts

--- src/app/intro-setup/intro-setup.component.ts
+++ src/app/intro-setup/intro-setup.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IntroSetupComponent implements OnInit {
 
+  isCompletedWifi=true
+  isCompletedLocation= true
+
   constructor() { }
 
   ngOnInit() {
